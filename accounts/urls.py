abbb1785e95dcc904f55bd6847fc10ce0f7a2414
alchemist1111 <emoyoeugine@gmail.com>
2025-10-01 17:57:56 +0300

    Add register and login urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .views import UserRegistrationAPIView, UserLoginAPIView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -10,4 +11,10 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    
+    # Register
+    path('register/', UserRegistrationAPIView.as_view(), name='user-registration'),
+    
+    # Login
+    path('login/', UserLoginAPIView.as_view(), name='user-login'),
 ]
