d08a2947911ef2ef486123310ce15adab3a838e4
alchemist1111 <emoyoeugine@gmail.com>
2025-10-07 14:45:10 +0300

    Add Note, Quiz and Questions models

--- content_management/models.py
+++ content_management/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+# Class for Note
+class Note(models.Model):
+    pass
+
+
+# Class for quiz
+class Quiz(models.Model):
+    pass
+
+
+# Class for Question
+class Question(models.Model):
+    pass
